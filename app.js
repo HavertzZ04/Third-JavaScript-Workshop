c1008b5c08ac1c81d96a3b28668b2eff5833bf84
Johan Carvajal <125088952+HavertzZ04@users.noreply.github.com>
2023-03-16 13:25:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 const select = document.querySelector("#options");
 const camperForm = document.querySelector("#camper");
 const trainerForm = document.querySelector("#trainer");
-const main = document.querySelector('.main');
 
-const procesaTodo = (event) => {
-    event.preventDefault();
-const datos = new FormData(event.target);
-const datosCompletos = Object.fromEntries(datos.entries());
-console.table(datosCompletos);
-}
-main.addEventListener('submit', procesaTodo);
-
-//Function that display the right form whrn users seclect between camper or trainer
 select.addEventListener("change", function(){
     if(select.value === "camper"){
         camperForm.style.display = "block"
         if(1 == 1){
             trainerForm.style.display = "none"
         }
-        
+
     }
     if (select.value === "trainer") {
         trainerForm.style.display = "block"
@@ -26,11 +16,19 @@ select.addEventListener("change", function(){
             camperForm.style.display = "none"
         }
     }
-    
+
 });
 
 
+const main = document.querySelector('.main');
 
+const procesaTodo = (event) => {
+    event.preventDefault();
+const datos = new FormData(event.target);
+const datosCompletos = Object.fromEntries(datos.entries());
+console.log(datosCompletos);}
+
+main.addEventListener('submit', procesaTodo);
 
 /*
 let campus = { //This is the main object
@@ -60,5 +58,6 @@ let campus = { //This is the main object
     }
 }
 */
+
 
 
