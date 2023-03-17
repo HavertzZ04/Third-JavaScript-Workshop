eaa2a91da639ad703b12a5fb04e210aa2c814f3a
HavertzZ04 <havertzz04@gmail.com>
2023-03-17 13:02:53 -0500

    feat: :zap: add JSON format
    The code prints on the console the information of each person in JSON format, then using split I got each each property with its element in order to add each on a array to store information

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const select = document.querySelector("#options");
 const camperForm = document.querySelector("#camper");
 const trainerForm = document.querySelector("#trainer");
 
+//Funcion que nos ayuda a imprimir en pantalla el formulario correcto segun si es camper or trainer
 select.addEventListener("change", function(){
     if(select.value === "camper"){
         camperForm.style.display = "block"
         if(1 == 1){
             trainerForm.style.display = "none"
         }
-
     }
     if (select.value === "trainer") {
         trainerForm.style.display = "block"
@@ -16,48 +16,20 @@ select.addEventListener("change", function(){
             camperForm.style.display = "none"
         }
     }
-
 });
 
-
+//Imprimir los valores del formulario en formato json, usando un split para separar cada propiedad con su valor
 const main = document.querySelector('.main');
-
 const procesaTodo = (event) => {
     event.preventDefault();
 const datos = new FormData(event.target);
 const datosCompletos = Object.fromEntries(datos.entries());
-console.log(datosCompletos);}
-
-main.addEventListener('submit', procesaTodo);
-
-/*
-let campus = { //This is the main object
-    Campus : {
-        city : [],
-    },
-    trainers: {
-        names : [],
-        phone : [],
-        email : [],
-        role : [],
-        schedule : [],
-        neighborhood : [],
-        transportation : [],
-    },
-    campers: {
-        names : [],
-        phone : [],
-        team : [],
-        map : [],
-        schedule : [],
-        programing : [],
-        english : [],
-        ser : [],
-        email : [],
-        level : []
-    }
+let jsonResult = JSON.stringify(datosCompletos);
+console.log(jsonResult);
+let jsonSplit = jsonResult.split(",")
+console.log(jsonSplit[1]);
 }
-*/
+main.addEventListener('submit', procesaTodo);
 
 
 
